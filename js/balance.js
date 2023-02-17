a72fae7e01615ee8ae8cf1a365ccd53fd34fd932
aanikbsmrstucse <aanikbsmrstucse@gmail.com>
2023-02-17 12:19:15 +0600

    all done

--- js/balance.js
+++ js/balance.js
@@ -1,17 +1,43 @@
+const totalBalance = document.getElementById("total-balance");
+const totalBalanceGet = totalBalance.innerText;
+const totalBalanceGetNew = parseFloat(totalBalanceGet);
 // first step add event listner in deposit button
-document.getElementById('btn-deposit').addEventListener('click',function(){
-    // get the value from user deposit input
-    const userDeposit = document.getElementById('user-deposit');
-    const userDepositAmount = userDeposit.value;
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  // get the value from user deposit input
+  const userDeposit = document.getElementById("user-deposit");
+  const userDepositAmount = userDeposit.value;
+
+  // set the value in user deposite display button
+  const depositDisplay = document.getElementById("deposit-display");
+  const depositDisplayAmount = depositDisplay.innerText;
+  const totalDepositAmount =
+    parseFloat(userDepositAmount) + parseFloat(depositDisplayAmount);
+  depositDisplay.innerText = totalDepositAmount;
+
+  const inTotalBalance = totalBalanceGetNew + totalDepositAmount;
+  totalBalance.innerText = inTotalBalance;
+  userDeposit.value = "";
+});
+
+// start working withdraw section
+//first step add event listner in withdraw button
+document
+  .getElementById("btn-withdraw")
+  .addEventListener("click", function () {
+    const userWithdraw = document.getElementById('user-withdraw');
+    const userWithdrawValue = parseFloat (userWithdraw.value);
+
+    const withdrawAmount = document.getElementById('withdraw-amount');
+    const withdrawAmountGet = parseFloat (withdrawAmount.innerText);
     
-    // set the value in user deposite display button
-    const depositDisplay = document.getElementById('deposit-display');
-    const depositDisplayAmount = depositDisplay.innerText;
-    const totalDepositAmount = parseFloat(userDepositAmount) + parseFloat(depositDisplayAmount);
-    depositDisplay.innerText = totalDepositAmount;
+    const totalWithdrawBalance = userWithdrawValue + withdrawAmountGet;
+    withdrawAmount.innerText = totalWithdrawBalance;
+
+    const totalBalanceAfterWithdraw = totalBalanceGetNew - totalWithdrawBalance;
+    totalBalance.innerText = totalBalanceAfterWithdraw;
+
+    userWithdraw.value = '';
     
-    userDeposit.value = '';
+  });
 
-})
-
-
+//start adding total ammount in balance
